e540bc1e11474f470fb21d0b9301a8b3b423f52c
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2025-04-10 15:42:55 -0300

    feat: correção de alguns erros

--- src/controllers/produtoController.js
+++ src/controllers/produtoController.js
@@ -39,7 +39,7 @@ class ProdutosController {
         return res.status(400).json({ erro: "Preço do produto é obrigatório!" });
       }
       if (!category) {
-        return res.status(400).json({ erro: "Categoria do produto é obrigatório!" });
+        return res.status(400).json({ erro: "Categoria do produto é obrigatória!" });
       }
       if (!brand) {
         return res.status(400).json({ erro: "Marca do produto é obrigatória!" });
@@ -70,7 +70,7 @@ class ProdutosController {
     const { name, price, category, brand, stock, imageUrl, isActive } = req.body;
 
     try {
-      const produtoAtualizada = await produtoModel.update(
+      const produtoAtualizado = await produtoModel.update(
         Number(id),
         name,
         price,
@@ -81,11 +81,11 @@ class ProdutosController {
         isActive
       );
 
-       if (!produtoAtualizada) {
+       if (!produtoAtualizado) {
         return res.status(404).json({ erro: "Produto não encontrada!" });
        }
 
-       res.json(produtoAtualizada);
+       res.json(produtoAtualizado);
     } catch (error) {
       console.error(error);
       res.status(500).json({ erro: "Erro ao atualizar produto!" });
